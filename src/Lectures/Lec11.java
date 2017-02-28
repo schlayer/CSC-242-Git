2398cd7a0770309ee9302e8e5f3c4780d753d2d3
Seth Schaffer <qwertbochaffer@gmail.com>
2017-02-28 10:18:11 -0500

    Update lec11

--- src/Lectures/Lec11.java
+++ src/Lectures/Lec11.java
@@ -29,13 +29,51 @@ public class Lec11 {
 	Knowledge Base - the rules we are given?
 	
 	
-	Past Propositional Logic
+			Past Propositional Logic
+	Pros:	
+		Declarative: Pl based on truth relations between sentences and possible worlds
+		Expressive: represents partial information
+		Compositional: meaning of a sentence is a simple function of the meaning of its parts
+	Cons:
+		Model checking takes exponential time - very inefficient
+		Lacks the power to concisely describe environments with many objects
 	
 	
+	Need to add something to actually represent general rules, since they need to be applied
+	to an entire world.
 	
 	
+	Logic 2.0: New language based on propositional logic that will allow us to say all these things
+		Define entailment (what "follows from" what)
+		Find inference rues allowing computation of consequences of knowledge
 	
 	
+	'Rooms adjacent to pits are breezy.'
+	'Socrates us a person. All people are mortals.'
+	'Anybody's grandmother i either their mother's or their father's mother.'
+	
+	Objects, relations, functions. "Ontology" of things.
+	
+	Objects:
+		nouns basically
+	Relations:
+		Unary relations/properties (breezy, mortal, red, round, prime... Adjectives
+		n-ary: Brother to, bigger than, part of, inside, has color, owns, above
+		Functions: "Single-valued" relations
+	
+	Block world:
+		Objects: Blocks
+		Relations: Being on, above, clear, on table
+		Functions: Block above me
+	
+	===========================================================
+	
+	A Programming Language for Knowledge
+	
+	Object Symbols denote/indicate objects (like letters/numbers)
+	Predicate Symbols denote relations [Mortal(.), Above(.,.)]
+	Function Symbols denote functions [mother(.), plus(.,.)]
+		Arity - number of arguments
 	
 	
 	
